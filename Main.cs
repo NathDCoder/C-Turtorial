6318e11910393ea97cafecef45c49f4c4e33d443
NathDCoder <themusicemperor@gmail.com>
2023-01-17 21:47:11 -0500

    C#

---
+++ Main.cs
@@ -0,0 +1,15 @@
+
+
+public class LetsGetIt
+{
+    public static void Main()
+    {
+        int a = 1;
+        int b = 2;
+
+        int c = a + b;
+
+        System.Console.WriteLine("Result is " + c);
+          
+    }
+}
